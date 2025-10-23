b7d3a328fa656d36a2763b4116b83343ef8a773b
Dhruba <gdhruba748+github@gmail.com>
2025-10-23 18:16:31 +0530

    api controllers for authentications

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+// user authentication
+export const loginUser = (req, res) => {
+  res.send("login route");
+};
+
+export const logoutUser = (req, res) => {
+  res.send("login route");
+};
+
+export const signupUser = (req, res) => {
+  res.send("login route");
+};
