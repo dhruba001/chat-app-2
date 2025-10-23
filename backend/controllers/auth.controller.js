8ccd125b45d3cc68493e9f3ea3478469e154eb4d
Dhruba <gdhruba748+github@gmail.com>
2025-10-23 22:34:28 +0530

    signup position in code changed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,12 @@
 // user authentication
+export const signupUser = (req, res) => {
+  res.send("signup route");
+};
+
 export const loginUser = (req, res) => {
   res.send("login route");
 };
 
 export const logoutUser = (req, res) => {
   res.send("logout route");
-};
-
-export const signupUser = (req, res) => {
-  res.send("signup route");
 };
