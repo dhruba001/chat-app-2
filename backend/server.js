666e72437402dced4a2f2fe24252f496a995f851
Dhruba <gdhruba748+github@gmail.com>
2025-10-23 23:51:52 +0530

    file modified

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
+//package imports
 import express from "express";
 import dotenv from "dotenv";
+//file imports
 import connectToMongoDB from "./DB/connectToMongoDB.js";
 import authRoutes from "../backend/routes/auth.routes.js";
+//variables
+const app = express(); // making express app instance
+const PORT = process.env.PORT || 5000; // getting port from env variable
 
-const app = express();
+// it loads environment variables from a file named .env into process.env.
+// Then these values become available throughout your code as environment variables:
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  // root route http://localhost:5000/
-  res.send("Hello world !!");
-});
-
-app.use("/api/auth", authRoutes);
+app.use(express.json()); // middleware : allows server to understand and handle data sent in JSON format from frontend in backend
+//from req.body as frontend data gets to backend in json format
+app.use("/api/auth", authRoutes); // middleware
 
 app.listen(PORT, () => {
   connectToMongoDB();
