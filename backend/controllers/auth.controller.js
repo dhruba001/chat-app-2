cac40546cc53d24b1dafffea30a747dfbf7de19a
Dhruba <gdhruba748+github@gmail.com>
2025-10-23 23:21:59 +0530

    important comments

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,23 @@
 // user authentication
-export const signupUser = (req, res) => {
-  res.send("signup route");
+// will be used as a api descriptiosn for authentication api's only
+
+import User from "../models/user.model";
+
+// descriptions of api's used in authentication part
+
+//signup api
+export const signupUser = async (req, res) => {
+  try {
+    const { firstName, userNmae, password, gender } = req.body; // getting data as user enters in frontend form
+  } catch (error) {}
 };
 
+//login user api
 export const loginUser = (req, res) => {
   res.send("login route");
 };
 
+//logout user api
 export const logoutUser = (req, res) => {
   res.send("logout route");
 };
