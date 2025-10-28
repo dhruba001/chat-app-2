599dc3a5dd2bebd753f7b1d1d048be4d1a2e09d5
Dhruba <gdhruba748+github@gmail.com>
2025-10-28 21:50:42 +0530

    setting new api for getting message

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { sendMessage } from "../controllers/message.controller.js";
+import { sendMessage, getMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
+// api routes
+
+// get message between a random user and loggedin user
+router.get("/:id", protectRoute, getMessages);
+// send message between any user and loggedin user
 router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
