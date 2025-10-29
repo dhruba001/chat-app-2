d0aa3a1749940391eb49c3a4e4174d0ed4faf81b
Dhruba <gdhruba748+github@gmail.com>
2025-10-29 15:53:12 +0530

    now rendering home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import Login from "./pages/login/Login.jsx";
-import SignUp from "./pages/signup/SignUp.jsx";
+import Home from "./pages/home/Home.jsx";
+// import Login from "./pages/login/Login.jsx";
+// import SignUp from "./pages/signup/SignUp.jsx";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <SignUp />
+      <Home />
     </div>
   );
 }
